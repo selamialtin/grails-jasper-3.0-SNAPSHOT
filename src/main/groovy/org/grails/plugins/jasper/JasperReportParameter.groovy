c8ac8f30a3670cf3cd511c1c3be44046c9874284
selamialtin <selamialtin@gmail.com>
2015-06-27 15:54:29 +0300

    1

---
+++ src/main/groovy/org/grails/plugins/jasper/JasperReportParameter.groovy
@@ -0,0 +1,16 @@
+package org.grails.plugins.jasper
+
+class JasperReportParameter {
+
+	String name
+	
+	String description
+	
+	Class<?> valueClass
+
+	String valueClassName
+	
+	Class<?> nestedType
+	
+	String nestedTypeName
+}
